56c0aa4ef37bd8d279899c3cfbd83ab8095dcd27
ayushrajput545 <rajputayush694@gmail.com>
2025-07-19 17:23:40 +0530

    replace skeleton loading with css loader

--- app/loading.js
+++ app/loading.js
@@ -1,10 +1,13 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Loading(){
-
-    return(
-        <div className="flex flex-col space-y-3">
-            <Skeleton className='min-h-[630px] h-full w-full bg-zinc-500 mt-5'/>
-        </div>
-    )
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex gap-5 flex-col items-center space-y-3">
+        {/* <Skeleton className='min-h-[630px] h-full w-full bg-zinc-500 mt-5'/> */}
+        <span className="loader"></span>
+        <p className="text-black text-xl font-bold">Loading, please wait...</p>
+      </div>
+    </div>
+  );
 }
