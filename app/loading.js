0ca35330a8f73391f537838f45f075dd0135264c
ayushrajput545 <rajputayush694@gmail.com>
2025-07-19 17:38:17 +0530

    fix loader for small screens

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen mb-[20vh] md:mb-0">
       <div className="flex gap-5 flex-col items-center space-y-3">
         {/* <Skeleton className='min-h-[630px] h-full w-full bg-zinc-500 mt-5'/> */}
         <span className="loader"></span>
